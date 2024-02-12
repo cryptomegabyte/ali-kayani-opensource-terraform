4cf64ab1a0fd6ee53fe25b012d76147b26cdb498
Ali Kayani <akayaniwork@outlook.com>
2024-02-12 10:47:28 +0000

    Corrected typo

--- wordpress_azure/resource-group.tf
+++ wordpress_azure/resource-group.tf
@@ -1,5 +1,5 @@
 # This resource block creates an Azure resource group using the azurecag_name resource type.
-resource "azurecag_name" "resource_group" {
+resource "azurecaf_name" "resource_group" {
     name = "var.name"
     resource_type = "azurerm_resource_group"
     suffixes = [
